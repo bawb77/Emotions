a91422217384924347aa86998e55334162bd8051
bawb77 <sandeniremailer@gmail.com>
2014-10-24 10:59:21 -0700

    String and second activity

--- EmotionsApp/gen/com/example/emotions/R.java
+++ EmotionsApp/gen/com/example/emotions/R.java
@@ -25,18 +25,42 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_start_screen=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int start_screen=0x7f070001;
     }
     public static final class string {
+        public static final int accept=0x7f05000c;
         public static final int action_settings=0x7f050002;
+        public static final int angry=0x7f050008;
+        public static final int answer=0x7f050004;
+        public static final int anxious=0x7f05000a;
         public static final int app_name=0x7f050000;
+        public static final int critisim=0x7f05000e;
+        public static final int depressed=0x7f05000b;
+        public static final int distraction=0x7f050012;
+        public static final int expsup=0x7f050011;
+        public static final int felsup=0x7f050014;
+        public static final int happy=0x7f050006;
         public static final int hello_world=0x7f050001;
+        public static final int problem=0x7f050013;
+        public static final int relaxed=0x7f050007;
+        public static final int reprise=0x7f05000d;
+        public static final int rumination=0x7f05000f;
+        public static final int sharing=0x7f050010;
+        public static final int since=0x7f050005;
+        public static final int stressed=0x7f050009;
+        public static final int title_activity_start_screen=0x7f050003;
+        public static final int wantc=0x7f050017;
+        public static final int wantp=0x7f050015;
+        public static final int wantu=0x7f050016;
     }
     public static final class style {
         /** 
